839af54512ab1c79243f9708c5eb0695724bdc8b
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-10-10 16:27:12 +0630

    learn: use ref

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,34 +1,10 @@
-import {useReducer} from "react";
+import {useRef} from "react";
 
-const countReducer = (state, action) => {
-    switch (action.type) {
-        case 'ADD':
-            return {
-                count: state.count + 1,
-                isOdd: state.count % 2 === 0
-            }
-        case 'REMOVE':
-            return {
-                count: state.count - 1,
-                isOdd: state.count % 2 === 0
-            }
-    }
-}
 const App = () => {
-    const [state, dispatchCount] = useReducer(countReducer, {
-        count: 0,
-        isOdd: false
-    })
-    return (
-        <div className="container my-4 d-flex flex-column">
-            <h1>{state.count}</h1>
-            <p>{state.isOdd ? 'Odd' : 'Even'}</p>
-            <div>
-                <button className="btn btn-danger mx-2" onClick={() => dispatchCount({type: 'REMOVE'})}>-</button>
-                <button className="btn btn-success" onClick={() => dispatchCount({type: 'ADD'})}>+</button>
-            </div>
-        </div>
-    )
+    const btnRef = useRef()
+
+    return <button ref={btnRef} onClick={() => btnRef.current.style.backgroundColor = 'green'}>change bg color</button>
+
 }
 
 export default App
